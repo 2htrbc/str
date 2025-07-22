2d20c54714b75f2ec2249499d852a6b4f0eda520
pd <honortv24@gmail.com>
2025-07-22 18:49:01 +0800

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const streamList = {
   1: 'https://amg00627-amg00627c30-rakuten-es-3990.playouts.now.amagi.tv/playlist/amg00627-banijayfast-mrbeanescc-rakutenes/playlist.m3u8',
   2: 'https://cdn-apse1-prod.tsv2.amagi.tv/linear/amg01076-lightningintern-kartoonchannel-samsungnz/playlist.m3u8',
   3: 'https://stream-us-east-1.getpublica.com/playlist.m3u8?network_id=50',
+  4: 'https://qp-pldt-live-grp-02-prod.akamaized.net/out/u/tv5.m3u8',
 };
 
 // CORS + Logging
@@ -61,9 +62,10 @@ app.get('/', (req, res) => {
     <h2>HLS Proxy Server</h2>
     <p>Use the links below:</p>
     <ul>
-      <li><a href="/playlist.m3u8?stream=1">Stream 1</a></li>
-      <li><a href="/playlist.m3u8?stream=2">Stream 2</a></li>
-      <li><a href="/playlist.m3u8?stream=3">Stream 3</a></li>
+      <li><a href="/playlist.m3u8?mrbean">Stream 1</a></li>
+      <li><a href="/playlist.m3u8?kartoon-channel">Stream 2</a></li>
+      <li><a href="/playlist.m3u8?kidsflix">Stream 3</a></li>
+      <li><a href="/playlist.m3u8?a2z">Stream 4</a></li>
     </ul>
   `);
 });
