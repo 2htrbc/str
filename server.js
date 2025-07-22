a8135d38845368dd17c4365ac44a9ad80e65592d
pd <honortv24@gmail.com>
2025-07-22 16:28:18 +0800

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 
 // Your stream URLs (HLS master or variant .m3u8)
 const streamList = {
-  1: 'https://2-fss-2.streamhoster.com/pl_140/amlst:200914-1298290/chunklist_b2000000.m3u8',
+  1: 'https://amg00627-amg00627c30-rakuten-es-3990.playouts.now.amagi.tv/playlist/amg00627-banijayfast-mrbeanescc-rakutenes/playlist.m3u8',
   2: 'https://cdn-apse1-prod.tsv2.amagi.tv/linear/amg01076-lightningintern-kartoonchannel-samsungnz/playlist.m3u8',
   3: 'https://stream-us-east-1.getpublica.com/playlist.m3u8?network_id=50',
 };
